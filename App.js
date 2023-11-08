ba8f7325184ec32fc7aceddc156ec04658b9d023
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2023-11-08 07:05:43 +0100

    Update App.js
    changed class => className which fixed the console dom-react error

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-
 const Button = (props) => {
-    return <button class="slide-btn">{props.symbol}</button>
+    return <button className="slide-btn">{props.symbol}</button>
 }
 
 const Slide = () => {
